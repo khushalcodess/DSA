6b8f5944670f1959ceef3e59bcf95fbc6cfa758c
Khushal Patil <khushalpatil3055@gmail.com>
2026-08-02 23:05:16 +0530

    Largest Element

---
+++ sorting/q8.java
@@ -0,0 +1,31 @@
+/*
+Largest Element
+Given an array of integers nums, return the value of the largest element in the array
+Example 1
+Input: nums = [3, 3, 6, 1]
+Output: 6
+Explanation: The largest element in array is 6
+*/
+
+import java.util.Arrays;
+
+/**
+ * q8
+ */
+public class q8 {
+     public int largestElement(int[] nums) {
+        int num = nums[0];
+        for(int i = 1 ; i<nums.length;i++){
+            if(num<nums[i]){
+                num = nums[i];
+            }
+        }
+        return num;
+    }
+    public static void main(String[] args) {
+        int[] nums = {3,3,6,1};
+        q8 obj = new q8();
+        obj.largestElement(nums);
+        System.out.println("Sorted Array: " + Arrays.toString(nums));
+    }
+}
