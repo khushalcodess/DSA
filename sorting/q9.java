a8c46ca1f0a973e9f3ce45a000b6964704aebd0e
Khushal Patil <khushalpatil3055@gmail.com>
2026-08-03 14:51:03 +0530

    Second Largest Element

---
+++ sorting/q9.java
@@ -0,0 +1,26 @@
+
+/**
+ * q9
+ */
+public class q9 {
+    public int secondLargestElement(int[] nums) {
+        int largest = nums[0] ;
+        int seclargest = -1;
+
+        for(int i = 1; i<nums.length; i++){
+            if(nums[i]>largest){
+                seclargest = largest;
+                largest = nums[i];
+            }
+            else if(nums[i]>seclargest && nums[i]!=largest){
+                seclargest = nums[i];
+            }
+        }
+        return seclargest;
+    }
+    public static void main(String[] args) {
+        int[] num = {8, 8, 7, 6, 5};
+        q9 obj = new q9();
+        System.err.println("Second largest number is :-"+obj.secondLargestElement(num));
+    }
+}
