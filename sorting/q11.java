35a9751a84bfd30ab94d3be583c4c90022607f4a
Khushal Patil <khushalpatil3055@gmail.com>
2026-08-06 22:50:05 +0530

    Remove duplicates from sorted array

---
+++ sorting/q11.java
@@ -0,0 +1,49 @@
+/*
+Remove duplicates from sorted array
+Example 1
+
+Input: nums = [0, 0, 3, 3, 5, 6]
+
+Output: 4
+
+Explanation:
+
+Resulting array = [0, 3, 5, 6, _, _]
+
+There are 4 distinct elements in nums and the elements marked as _ can have any value
+*/
+
+class q11 {
+
+    public int removeDuplicates(int[] nums) {
+
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) {
+
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+
+        q11 obj = new q11();
+
+        int[] nums = {0, 0, 3, 3, 5, 6};
+
+        int k = obj.removeDuplicates(nums);
+
+        System.out.println("Number of unique elements: " + k);
+
+        System.out.print("Array after removing duplicates: ");
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
