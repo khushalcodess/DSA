3c72c94503554d7fd7a418bda362f3722f23678e
Khushal Patil <khushalpatil3055@gmail.com>
2026-08-09 17:07:12 +0530

    Updated version Left Rotate Array by k Place

--- sorting/q13.java
+++ sorting/q13.java
@@ -15,31 +15,17 @@ import java.util.*;
 class Solution {
 
     public void rotateArray(int[] nums, int k) {
-
         int n = nums.length;
-
-        k = k % n;
-
-
-        reverse(nums, 0, k - 1);
-
-        reverse(nums, k, n - 1);
-
-
-        reverse(nums, 0, n - 1);
-    }
-
-
-    public void reverse(int[] nums, int start, int end) {
-
-        while (start <= end) {
-
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-
-            start++;
-            end--;
+        k = k % n ;
+        int[] temp = new int[k];
+        for(int i = 0; i < k; i++){
+            temp[i] = nums[i];
+        }
+        for(int i = k ; i < n ; i++ ){
+            nums[i-k] = nums[i];
+        }
+        for(int i = n - k ; i < n ; i++ ){
+            nums[i] = temp[i-(n - k)];
         }
     }
 
