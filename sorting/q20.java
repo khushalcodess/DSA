c951afd0c11f7625b703c3da3a58ada77884c007
Khushal Patil <khushalpatil3055@gmail.com>
2026-08-18 21:17:24 +0530

    Solved Find missing number with optimal approach

---
+++ sorting/q20.java
@@ -0,0 +1,33 @@
+
+/*
+Find missing number
+Input: nums = [0, 2, 3, 1, 4]
+
+Output: 5
+
+Explanation:
+
+nums contains 0, 1, 2, 3, 4 thus leaving 5 as the only missing number in the range [0, 5]
+*/
+package sorting;
+
+public class q20 {
+    public static  int missingNumber(int[] nums) {
+        int sum = (nums.length)*(nums.length + 1)/2;
+        int s2 = 0;
+
+        for (int i = 0 ; i<nums.length; i++){
+            s2 += nums[i];
+        }
+
+        return sum - s2;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5};
+
+        int answer = missingNumber(arr);
+
+        System.out.println("Missing number = " + answer);
+
+    }
+}
