00c30983529cc1fe598879defa51db70c5eb47a5
Khushal Patil <khushalpatil3055@gmail.com>
2026-08-19 14:28:19 +0530

    Solved Single Number - I

---
+++ sorting/q22.java
@@ -0,0 +1,26 @@
+/*
+Single Number - I
+Input : nums = [1, 2, 2, 4, 3, 1, 4]
+Output : 3
+Explanation : The integer 3 has appeared only once.
+*/
+
+package sorting;
+
+public class q22 {
+    public static  int singleNumber(int[] nums) {
+        int xor =0;
+
+        for(int i = 0; i<nums.length;i++){
+            xor = xor ^ nums[i];
+        }
+        return xor;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 0, 1, 1, 1, 0};
+
+        int answer = singleNumber(arr);
+
+        System.out.println("Single Number = " + answer);
+    }
+}
