928eed41324d0a6961bc81e8a3a77861ac5f8f64
Khushal Patil <khushalpatil3055@gmail.com>
2026-09-02 23:45:21 +0530

    Solved Majority Element-I with Brute Force Approach

---
+++ Arrays/q24.java
@@ -0,0 +1,53 @@
+/*
+Majority Element-I
+Example 1
+
+Input: nums = [7, 0, 0, 1, 7, 7, 2, 7, 7]
+
+Output: 7
+
+Explanation:
+
+The number 7 appears 5 times in the 9 sized array
+*/
+class q24 {
+
+    public int majorityElement(int[] nums) {
+
+        int n = nums.length;
+
+
+        for (int i = 0; i < n; i++) {
+
+            int count = 0;
+
+
+            for (int j = 0; j < n; j++) {
+
+                if (nums[j] == nums[i]) {
+                    count++;
+                }
+            }
+
+
+            if (count > n / 2) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+
+
+    // Main method
+    public static void main(String[] args) {
+
+        q24 obj = new q24();
+
+        int[] nums = {7, 0, 0, 1, 7, 7, 2, 7, 7};
+
+        int answer = obj.majorityElement(nums);
+
+        System.out.println("Majority Element: " + answer);
+    }
+}
