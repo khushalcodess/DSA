6daae2d583aaf527c0d61b3fe4db853bb661eff4
Khushal Patil <khushalpatil3055@gmail.com>
2026-09-17 22:58:45 +0530

    Solved Leaders in an Array with Optimal Approach

--- Arrays/q35.java
+++ Arrays/q35.java
@@ -1,5 +1,5 @@
 /*
-Brute Approach
+Optimal Approach
 Leaders in an Array
 Given an integer array nums, return a list of all the leaders in the array.
 A leader in an array is an element whose value is strictly greater than all elements to its right in the given array. The rightmost element is always a leader. The elements in the leader array must appear in the order they appear in the nums array.
@@ -13,30 +13,33 @@ Explanation:
 
 import java.util.*;
 
-public class q34 {
+public class q35 {
 
-    // Brute Force Approach
     public static List<Integer> leaders(int[] nums) {
 
         List<Integer> ans = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++) {
+     
+        int n = nums.length;
+        int max = nums[n - 1];
 
-            boolean isLeader = true;
+        ans.add(max);
 
-            for (int j = i + 1; j < nums.length; j++) {
+      
+        for (int i = n - 2; i >= 0; i--) {
 
-                if (nums[i] <= nums[j]) {
-                    isLeader = false;
-                    break;
-                }
-            }
-
-
-            if (isLeader) {
+       
+            if (nums[i] > max) {
                 ans.add(nums[i]);
+
+       
+                max = nums[i];
             }
         }
+
+    
+
+        Collections.reverse(ans);
 
         return ans;
     }
